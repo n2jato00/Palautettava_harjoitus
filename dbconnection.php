c3cdc5552dfc18b2836eb3a54773744a4cf34da6
n2jato00 <n2jato00@students.oamk.fi>
2023-04-13 13:05:26 +0300

    mysli not work so change code to xampp

--- dbconnection.php
+++ dbconnection.php
@@ -2,16 +2,15 @@
 
 function createDbConnection()
 {
-    $ini = parse_ini_file('myconf.ini');
+/*     $ini = parse_ini_file('myconf.ini');
 
     $host = $ini['host'];
     $db = $ini['db'];
     $username = $ini['username'];
-    $pw = $ini['pw'];
+    $pw = $ini['pw']; */
 
     try {
-        $dbcon = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $username, $pw);
-        echo "Connected to $db at $host successfully.";
+        $dbcon = new PDO('mysql:host=localhost;dbname=chinook;charset=utf8','root','');
         return $dbcon;
     } catch (PDOException $e) {
         echo $e->getMessage();
